eab96a4e9e0c71250c2c9e6ac00b214342498adf
desireemoon <desiree.dewysocki@gmail.com>
2019-11-08 16:42:57 -0500

    fixed route

--- social_tracker/client/src/App.js
+++ social_tracker/client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+// import axios from "axios";
 import './App.css';
 
 class App extends Component {
@@ -9,16 +10,41 @@ class App extends Component {
       .then(json => console.log(json))
       .catch(error => console.log(error));
   }
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     networks: [],
+  //     currentTeacher: {},
+  //     teachersLoaded: false
+  //   };
+  // }
+
+
+  // getAllTeachers = () => {
+  //   axios.get("/api/networks").then(jsonRes => {
+  //     this.setState({
+  //       networks: jsonRes.data.networks,
+  //       teachersLoaded: true
+  //     });
+  //   });
+  // };
+
+  // handleDeleteTeacher = (removedTeacher) => {
+  //   this.setState({
+  //     teachers: this.state.teachers.filter(teacher => teacher.id !== removedTeacher.id)
+  //   })
+  // }
+
+  // setTeacher = (teacher) => {
+  //   this.setState({
+  //     currentTeacher: teacher
+  //   });
+  // };
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
